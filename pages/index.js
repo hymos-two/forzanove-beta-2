9da0526dcaf7d9ff89dc8ff055e95c6fd59b3303
hymos-two <newdategettime@gmail.com>
2022-03-25 23:14:44 +0700

    setup layout

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,29 @@
+//components
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+//hooks
+import { useWindowSize } from '../src/hooks/useWindowSize';
 
 export default function Home() {
   return (
-    <div className="bg-blue w-full h-16">
+    <Container title="Forzanove 09">
+      <div className="grid grid-cols-8"></div>
+    </Container>
+  );
+}
+
+function Container(props) {
+  const size = useWindowSize();
+
+  return (
+    <div
+      className="font-sans bg-black-100 w-full"
+      style={{ height: size.height - 1 }}
+    >
       <Head>
-        <title>Create Next App</title>
+        <title>{props.title}</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-        </a>
-      </footer>
+      {props.children}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#32e379',
+        black: {
+          100: 'hsla(220,12%,5%,1)',
+          200: 'hsla(220,12%,8%,1)',
+          300: 'hsla(220,12%,10%,1)',
+          400: 'hsla(220,12%,15%,1)',
+          500: 'hsla(220,12%,20%,1)',
+          600: 'hsla(220,12%,25%,1)',
+        },
+      },
+    },
   },
   plugins: [],
 };
