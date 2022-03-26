1aa4b65af040f82a9bd7606795cea55ac1533f9a
hymos-two <newdategettime@gmail.com>
2022-03-26 13:25:23 +0700

    1 binaries uploaded

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 //components
 import Head from 'next/head';
+import Image from 'next/image';
 //hooks
 import { useWindowSize } from '../src/hooks/useWindowSize';
 
 export default function Home() {
   return (
     <Container title="Forzanove 09">
-      <div className="grid grid-cols-8">
-        <div className="col-span-3">Hello World</div>
-      </div>
+      <div className="grid grid-cols-8 h-screen relative bg-[url('../public/img/forzanove-bg.png')] bg-cover bg-center">
+        <div className="col-span-3 bg-black-100"></div></div>
+        <div className="absolute">
+          <Image src={forzanoveLogo} />
+        </div>
     </Container>
   );
 }
@@ -18,7 +21,7 @@ function Container(props) {
 
   return (
     <div
-      className="font-sans text-white bg-black-100 w-full"
+      className="font-sans text-white bg-black-100 w-full overflow-scroll"
       style={{ height: size.height - 1 }}
     >
       <Head>
